3dc5e4ad863c1ddca560912b470298a92e3e2fd2
webnetkz <8gg001@gmail.com>
2020-06-04 10:11:44 +0600

    update old code

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://happytoys.kz/admin/');
-define('HTTP_CATALOG', 'https://happytoys.kz/');
+define('HTTP_SERVER', 'https://dealworld.shop/admin/');
+define('HTTP_CATALOG', 'https://dealworld.shop/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://happytoys.kz/admin/');
-define('HTTPS_CATALOG', 'https://happytoys.kz/');
+define('HTTPS_SERVER', 'https://dealworld.shop/admin/');
+define('HTTPS_CATALOG', 'https://dealworld.shop/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/vhosts/happytoys.kz/httpdocs/admin/');
-define('DIR_SYSTEM', '/var/www/vhosts/happytoys.kz/httpdocs/system/');
-define('DIR_LANGUAGE', '/var/www/vhosts/happytoys.kz/httpdocs/admin/language/');
-define('DIR_TEMPLATE', '/var/www/vhosts/happytoys.kz/httpdocs/admin/view/template/');
-define('DIR_CONFIG', '/var/www/vhosts/happytoys.kz/httpdocs/system/config/');
-define('DIR_IMAGE', '/var/www/vhosts/happytoys.kz/httpdocs/image/');
-define('DIR_CACHE', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/download/');
-define('DIR_LOGS', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/upload/');
-define('DIR_CATALOG', '/var/www/vhosts/happytoys.kz/httpdocs/catalog/');
+define('DIR_APPLICATION', '/var/www/vhosts/dealworld.shop/httpdocs/admin/');
+define('DIR_SYSTEM', '/var/www/vhosts/dealworld.shop/httpdocs/system/');
+define('DIR_LANGUAGE', '/var/www/vhosts/dealworld.shop/httpdocs/admin/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/dealworld.shop/httpdocs/admin/view/template/');
+define('DIR_CONFIG', '/var/www/vhosts/dealworld.shop/httpdocs/system/config/');
+define('DIR_IMAGE', '/var/www/vhosts/dealworld.shop/httpdocs/image/');
+define('DIR_CACHE', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/download/');
+define('DIR_LOGS', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/vhosts/dealworld.shop/httpdocs/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'srv-pleskdb47.ps.kz:3306');
-define('DB_USERNAME', 'happytoy_xx');
-define('DB_PASSWORD', 'fepipe76!!');
-define('DB_DATABASE', 'happytoy_x');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'ghbdtn11!!QQ');
+define('DB_DATABASE', 'dealworld');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://happytoys.kz/');
+define('HTTP_SERVER', 'https://dealworld.shop/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://happytoys.kz/');
+define('HTTPS_SERVER', 'https://dealworld.shop/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/vhosts/happytoys.kz/httpdocs/catalog/');
-define('DIR_SYSTEM', '/var/www/vhosts/happytoys.kz/httpdocs/system/');
-define('DIR_LANGUAGE', '/var/www/vhosts/happytoys.kz/httpdocs/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/vhosts/happytoys.kz/httpdocs/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/vhosts/happytoys.kz/httpdocs/system/config/');
-define('DIR_IMAGE', '/var/www/vhosts/happytoys.kz/httpdocs/image/');
-define('DIR_CACHE', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/download/');
-define('DIR_LOGS', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/vhosts/happytoys.kz/httpdocs/system/storage/upload/');
+define('DIR_APPLICATION', '/var/www/vhosts/dealworld.shop/httpdocs/catalog/');
+define('DIR_SYSTEM', '/var/www/vhosts/dealworld.shop/httpdocs/system/');
+define('DIR_LANGUAGE', '/var/www/vhosts/dealworld.shop/httpdocs/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/dealworld.shop/httpdocs/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/vhosts/dealworld.shop/httpdocs/system/config/');
+define('DIR_IMAGE', '/var/www/vhosts/dealworld.shop/httpdocs/image/');
+define('DIR_CACHE', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/download/');
+define('DIR_LOGS', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/vhosts/dealworld.shop/httpdocs/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'srv-pleskdb47.ps.kz:3306');
-define('DB_USERNAME', 'happytoy_xx');
-define('DB_PASSWORD', 'fepipe76!!');
-define('DB_DATABASE', 'happytoy_x');
+define('DB_HOSTNAME', 'localhost:3306');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'ghbdtn11!!QQ');
+define('DB_DATABASE', 'dealworld');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
