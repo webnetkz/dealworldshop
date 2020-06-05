5f34d43fb4683602205ba8e6cdb549692db2fa13
root <root@dealworld.shop>
2020-06-05 16:06:10 +0600

    update version 0.02

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'https://dealworld.shop/admin/');
 define('HTTPS_CATALOG', 'https://dealworld.shop/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/vhosts/dealworld.shop/admin/');
-define('DIR_SYSTEM', '/var/www/vhosts/dealworld.shop/system/');
-define('DIR_LANGUAGE', '/var/www/vhosts/dealworld.shop/admin/language/');
-define('DIR_TEMPLATE', '/var/www/vhosts/dealworld.shop/admin/view/template/');
-define('DIR_CONFIG', '/var/www/vhosts/dealworld.shop/system/config/');
-define('DIR_IMAGE', '/var/www/vhosts/dealworld.shop/image/');
-define('DIR_CACHE', '/var/www/vhosts/dealworld.shop/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/vhosts/dealworld.shop/system/storage/download/');
-define('DIR_LOGS', '/var/www/vhosts/dealworld.shop/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/vhosts/dealworld.shop/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/vhosts/dealworld.shop/system/storage/upload/');
-define('DIR_CATALOG', '/var/www/vhosts/dealworld.shop/catalog/');
+define('DIR_APPLICATION', '/var/www/dealworld.shop/admin/');
+define('DIR_SYSTEM', '/var/www/dealworld.shop/system/');
+define('DIR_LANGUAGE', '/var/www/dealworld.shop/admin/language/');
+define('DIR_TEMPLATE', '/var/www/dealworld.shop/admin/view/template/');
+define('DIR_CONFIG', '/var/www/dealworld.shop/system/config/');
+define('DIR_IMAGE', '/var/www/dealworld.shop/image/');
+define('DIR_CACHE', '/var/www/dealworld.shop/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/dealworld.shop/system/storage/download/');
+define('DIR_LOGS', '/var/www/dealworld.shop/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/dealworld.shop/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/dealworld.shop/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/dealworld.shop/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'https://dealworld.shop/');
 define('HTTPS_SERVER', 'https://dealworld.shop/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/vhosts/dealworld.shop/catalog/');
-define('DIR_SYSTEM', '/var/www/vhosts/dealworld.shop/system/');
-define('DIR_LANGUAGE', '/var/www/vhosts/dealworld.shop/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/vhosts/dealworld.shop/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/vhosts/dealworld.shop/system/config/');
-define('DIR_IMAGE', '/var/www/vhosts/dealworld.shop/image/');
-define('DIR_CACHE', '/var/www/vhosts/dealworld.shop/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/vhosts/dealworld.shop/system/storage/download/');
-define('DIR_LOGS', '/var/www/vhosts/dealworld.shop/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/vhosts/dealworld.shop/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/vhosts/dealworld.shop/system/storage/upload/');
+define('DIR_APPLICATION', '/var/www/dealworld.shop/catalog/');
+define('DIR_SYSTEM', '/var/www/dealworld.shop/system/');
+define('DIR_LANGUAGE', '/var/www/dealworld.shop/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/dealworld.shop/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/dealworld.shop/system/config/');
+define('DIR_IMAGE', '/var/www/dealworld.shop/image/');
+define('DIR_CACHE', '/var/www/dealworld.shop/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/dealworld.shop/system/storage/download/');
+define('DIR_LOGS', '/var/www/dealworld.shop/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/dealworld.shop/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/dealworld.shop/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
